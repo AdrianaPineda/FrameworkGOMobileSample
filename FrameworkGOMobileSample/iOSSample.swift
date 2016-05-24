e37da216244fe35bea9573ae7784013019c1b410
Adriana Pineda <am.pineda206@uniandes.edu.co>
2016-05-24 11:19:03 -0500

    Add iOSSample class to use Sample.framework functionality

---
+++ FrameworkGOMobileSample/iOSSample.swift
@@ -0,0 +1,18 @@
+//
+//  iOSSample.swift
+//  FrameworkGOMobileSample
+//
+//  Created by Adriana Pineda on 5/24/16.
+//  Copyright © 2016 Adriana Pineda. All rights reserved.
+//
+
+import UIKit
+import Sample
+
+class iOSSample: NSObject {
+
+    func sum(a: Int32, b: Int32) -> Int32 {
+        return GoSampleSum(a, b)
+    }
+
+}
