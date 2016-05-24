8e1d33c5f112b521578bf5567b5e5a882baebcfa
Adriana Pineda <am.pineda206@uniandes.edu.co>
2016-05-24 10:19:39 -0500

    Initialize Xcode project

---
+++ FrameworkGOMobileSample/FrameworkGOMobileSample.h
@@ -0,0 +1,19 @@
+//
+//  FrameworkGOMobileSample.h
+//  FrameworkGOMobileSample
+//
+//  Created by Adriana Pineda on 5/24/16.
+//  Copyright © 2016 Adriana Pineda. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FrameworkGOMobileSample.
+FOUNDATION_EXPORT double FrameworkGOMobileSampleVersionNumber;
+
+//! Project version string for FrameworkGOMobileSample.
+FOUNDATION_EXPORT const unsigned char FrameworkGOMobileSampleVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FrameworkGOMobileSample/PublicHeader.h>
+
+
