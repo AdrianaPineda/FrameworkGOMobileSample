97188b815b965bb3802b8f3ec5c8d47ccf334e8f
Adriana Pineda <am.pineda206@uniandes.edu.co>
2016-05-26 09:04:11 -0500

    Import header from Sample.framework into project and change import statement

--- FrameworkGOMobileSample/FrameworkGOMobileSample.h
+++ FrameworkGOMobileSample/FrameworkGOMobileSample.h
@@ -16,4 +16,4 @@ FOUNDATION_EXPORT const unsigned char FrameworkGOMobileSampleVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <FrameworkGOMobileSample/PublicHeader.h>
 
-#import <Sample/Sample.h>
+#import "Sample.h"
